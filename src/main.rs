90c01975ed2f9559a569025b87bdfe7881e65e58
Bernard Riemann <riemann.br@gmail.com>
2023-11-18 18:17:31 +0100

    probcode generation

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,40 @@
 use std::{fs::read, collections::HashMap};
 use memchr::memmem;
 use std::cmp::min;
+use simple_bar::ProgressBar;
 
-fn read_file_line_by_line(filepath: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let file = read(filepath)?;
-    println!("file\n{}", String::from_utf8_lossy(&file[..200]));
+fn generate_probcodes(file: &[u8], markov_order: usize) -> Vec<u8> {
+    let mut bar = ProgressBar::default(file.len() as u32, 50, true);
 
-    for n in 199..200 {
-        let rotund = make_rotund(&file[..n]);
-        println!("{} rotund{} {}", n, rotund.len(), String::from_utf8_lossy(&rotund));
+    let mut probcodes: Vec<u8> = Vec::new();
+
+    bar.update();
+    for n in 1..file.len() {
+        //print!("n{} ", n);
+        let rotund = make_rotund(&file[..n], markov_order);
+        //println!("rotund{} {}", rotund.len(), String::from_utf8_lossy(&rotund[..16]));
+
+        let target_u8 = file[n]; 
+        probcodes.push( rotund.iter().position(|&x| x == target_u8).unwrap() as u8 );
+        bar.update();
     }
-    Ok(())
+    probcodes
 }
 
 
-fn make_rotund(content: &[u8]) -> Vec<u8> {
+fn make_rotund(content: &[u8], markov_order: usize) -> Vec<u8> {
     //rotund is a permutation of the range 0..256. It is ordered so that bytes with highest probability occur first.
     let mut rotund : Vec<u8> = Vec::new();
     let n = content.len();
-    let lemax = min(n, 10);
+    let lemax = min(n, markov_order);
     for le in (1..lemax).rev() {
         let pat = &content[n-le..n];
-
-        println!("le{} pat:{:?}", le, String::from_utf8_lossy(pat));
 
         let mut counter: HashMap<u8, usize> = HashMap::new();
 
         for itra in memmem::find_iter(&content[..n-1], &pat) {
             let next_u8 = content[itra+le];
-            println!("{:?} {:?} -> {}", itra, String::from_utf8_lossy(&content[itra..itra+le+1]), next_u8 as char);
+            //println!("{:?} {:?} -> {}", itra, String::from_utf8_lossy(&content[itra..itra+le+1]), next_u8 as char);
 
             if rotund.contains(&next_u8) {
                 continue
@@ -38,6 +44,14 @@ fn make_rotund(content: &[u8]) -> Vec<u8> {
         }
         let mut r: Vec<(&u8, &usize)> = counter.iter().collect();
         r.sort_by(|a, b| b.1.cmp(a.1));
+        if r.is_empty() {
+            continue
+        }
+        //print!("\nle{} pat:{:?}", le, String::from_utf8_lossy(pat));
+        //for etr in &r {
+            //print!(" {}: {}, ", *etr.0 as char, etr.1);
+        //}
+
         for ri in r {
             rotund.push(*ri.0);
         }
@@ -56,5 +70,10 @@ fn make_rotund(content: &[u8]) -> Vec<u8> {
 }
 
 fn main() {
-    let _ = read_file_line_by_line("../enwik9");
+    let file = read("../enwik9").unwrap();
+    //println!("file\n{}", String::from_utf8_lossy(&file[..1000]));
+    
+    let probcodes = generate_probcodes(&file[..100000], 200);
+    //println!("probcode: {:?}", probcodes);
+    std::fs::write("probcodes.u8", probcodes).unwrap();
 }
