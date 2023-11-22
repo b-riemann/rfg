c19134515e5d2a4e5a32fa4a30cea47e88e9efee
Bernard Riemann <riemann.br@gmail.com>
2023-11-22 20:07:33 +0100

    changed call syntax

--- src/main.rs
+++ src/main.rs
@@ -117,49 +117,52 @@ fn generate_probcodes(rfile: &[u8], markov_order: usize) -> Vec<u8> {
 }
 
 fn main() {
+    let mut args = env::args();
+    args.next();
+
     // next step: make weighted_rotund, which is now linear in length-to-proability, maybe quadratic in len and see what happens
-    for (narg, arg) in env::args().enumerate() {
-        if narg==0 { continue }
-        match arg.as_str() {
-            "slice<-enwik" => {
-                let maxlen = 400_000;
-                let file = read("../enwik9").unwrap();
-                std::fs::write("enwik.slice", &file[..maxlen]).unwrap();
-            },
-            "probcodes<-slice" => {
-                let mut file = read("enwik.slice").unwrap();
-                file.reverse();
-                let probcodes = generate_probcodes(&file, 1000);
-                std::fs::write("probcodes.u8", probcodes).unwrap();
-            },
-            "entropy<-probcodes" => {
-                let probcodes = std::fs::read("probcodes.u8").unwrap();
-                let mut slots = [0u32; 256];
-                for n in probcodes {
-                    slots[n as usize] += 1;
-                }
-                println!("{:?}", slots);
-                let mut entropy = 0.0;
-                let su: u32 = slots.iter().sum();
-                let sm = su as f64;
-                for s in slots.iter().filter(|&x| *x!=0) {
-                    let p = (*s as f64) / sm;
-                    entropy -= p * p.log2();
-                }
-                println!("{}", entropy);
-            },
-            "huffman<-probcodes" => {
-                let probcodes = std::fs::read("probcodes.u8").unwrap();
+    match args.next().unwrap().as_str() {
+        "slice<-enwik" => {
+            let maxlen = 400_000;
+            let file = read("../enwik9").unwrap();
+            std::fs::write("enwik.slice", &file[..maxlen]).unwrap();
+        },
+        "probcodes<-" => {
+            let filename = args.next().unwrap();
+            let mut file = read(filename).unwrap();
+            file.reverse();
+            let probcodes = generate_probcodes(&file, 1000);
+            std::fs::write("probcodes.u8", probcodes).unwrap();
+        },
+        "entropy<-" => {
+            let filename = args.next().unwrap();
+            let probcodes = read(filename).unwrap();
+            let mut slots = [0u32; 256];
+            for n in probcodes {
+                slots[n as usize] += 1;
+            }
+            //println!("{:?}", slots);
+            let mut entropy = 0.0;
+            let su: u32 = slots.iter().sum();
+            let sm = su as f64;
+            for s in slots.iter().filter(|&x| *x!=0) {
+                let p = (*s as f64) / sm;
+                entropy -= p * p.log2();
+            }
+            println!("entropy = {:.3} bits/byte", entropy);
+        },
+        "huffman<-" => {
+            let filename = args.next().unwrap();
+            let probcodes = std::fs::read(filename).unwrap();
 
-                let tree = huffman_coding::HuffmanTree::from_data(&probcodes);
-                let tree_table = tree.to_table();
-                std::fs::write("huffcodes.tree", tree_table).unwrap();
+            let tree = huffman_coding::HuffmanTree::from_data(&probcodes);
+            let tree_table = tree.to_table();
+            std::fs::write("huffcodes.tree", tree_table).unwrap();
 
-                let mut huffcodes = std::fs::File::create("huffcodes.bin").unwrap(); //Vec::new();
-                let mut writer = huffman_coding::HuffmanWriter::new(&mut huffcodes, &tree);
-                writer.write(&probcodes).unwrap();
-            },
-            x => println!("undefined mode: {}", x)
-        }
+            let mut huffcodes = std::fs::File::create("huffcodes.bin").unwrap(); //Vec::new();
+            let mut writer = huffman_coding::HuffmanWriter::new(&mut huffcodes, &tree);
+            writer.write(&probcodes).unwrap();
+        },
+        x => println!("undefined mode: {}", x)
     }
 }
